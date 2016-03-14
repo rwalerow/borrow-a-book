71bf160c081c4f296d1f90ea1c4a947dd4438c89
Robert Walerowicz <robert.walerowicz>
2016-03-14 20:43:27 +0100

    Working ts compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require('gulp');
 const del = require('del');
-const typescript = require('gulp-typescript');
+const ts = require('gulp-typescript');
 const tscConfig = require('./tsconfig.json');
+
 
 // clean the contents of the distribution directory
 gulp.task('clean', function () {
@@ -15,6 +16,22 @@ gulp.task('compile', ['clean'], function () {
     .pipe(typescript(tscConfig))
     .pipe(gulp.dest('public/javascripts/app'));
 });
+
+gulp.task('ts', function(done) {
+  var tsResult = gulp.src([
+      "node_modules/angular2/core.d.ts",
+      "node_modules/angular2/common.d.ts",
+      "node_modules/angular2/http.d.ts",
+      "node_modules/angular2/router.d.ts",
+      "node_modules/rxjs/Rx.d.ts",
+      "node_modules/angular2/typings/es6-collections/es6-collections.d.ts",
+      "node_modules/angular2/typings/es6-promise/es6-promise.d.ts",
+      "app/assets/app/**/*.ts"
+    ])
+    .pipe(ts(tscConfig), undefined, ts.reporter.fullReporter());
+  return tsResult.js.pipe(gulp.dest('public/javascripts'));
+});
+
 
 gulp.task('build', ['compile']);
 gulp.task('default', ['build']);
--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,4 +1,6 @@
 /// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="browser/ambient/es6-shim/index.d.ts" />
+/// <reference path="browser/ambient/jasmine/index.d.ts" />
 /// <reference path="browser/ambient/jasmine/jasmine.d.ts" />
 /// <reference path="es6-collections/es6-collections.d.ts" />
 /// <reference path="es6-promise/es6-promise.d.ts" />
