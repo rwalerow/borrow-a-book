ade15b5e1f29892d5f9effb539bf1dbfae589480
Robert Walerowicz <robert.walerowicz>
2016-03-14 21:28:06 +0100

    Added libs gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,20 @@ gulp.task('compile', ['clean'], function () {
     .pipe(gulp.dest('public/javascripts/app'));
 });
 
+gulp.task('libs', function () {
+    return gulp.src([
+          "node_modules/angular2/bundles/angular2.dev.js",
+          "node_modules/angular2/bundles/angular2-polyfills.min.js",
+          "node_modules/traceur/bin/traceur-runtime.js",
+          "node_modules/es6-module-loader/dist/es6-module-loader.js",
+          "node_modules/systemjs/dist/system.js",
+          "node_modules/es6-shim/es6-shim.js",
+          "node_modules/rxjs/Rx.js"
+        ])
+      .pipe(gulp.dest('public/javascripts/lib'));
+});
+
+
 gulp.task('ts', function(done) {
   var tsResult = gulp.src([
       "node_modules/angular2/core.d.ts",
@@ -32,6 +46,6 @@ gulp.task('ts', function(done) {
   return tsResult.js.pipe(gulp.dest('public/javascripts'));
 });
 
-
+gulp.task('front-dev', ['libs', 'ts'])
 gulp.task('build', ['compile']);
 gulp.task('default', ['build']);
