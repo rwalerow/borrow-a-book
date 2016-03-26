b1df6f680062d2bd4f6509f72bec4814348be1a7
robert <robert@localhost.localdomain>
2016-03-26 22:27:10 +0100

    Angular2 templateUrl proof of concept

--- app/assets/app/components/first.ts
+++ app/assets/app/components/first.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Hello world Asia 5</h1>'
+    templateUrl: 'assets/templates/first.html'
 })
 export default class AppComponent { }
